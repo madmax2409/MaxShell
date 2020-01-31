3e58400627fdf4295d9e4dd5146f4c151a11eebd
u101010 <u101010@deshalit.local>
2020-01-31 09:40:10 +0200

    file manager window attempt

---
+++ terminal graphics/terminal graphics/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace terminal_graphics
+{
+    public partial class Form2 : Form
+    {
+        TreeView tv = new TreeView();
+        public Form2()
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+
+            InitializeComponent();
+        }
+    }
+}
