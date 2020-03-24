1ae9b9f2b901745578a1f8ebe1c6015021593c07
Maxim Lyakhovitsky <maxim.lyakhovitsky@gmail.com>
2020-03-24 22:05:41 +0200

    cleanup and new objectives

--- server/WmiFuncs.cs
+++ server/WmiFuncs.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Management;
 using System.Net;
-using System.Net.Sockets;
 using System.IO;
 
 namespace server
