bea9fe70e4e6e6d00cf4ef0f1d2266c19b93de28
Maxim Lyakhovitsky <maxim.lyakhovitsky@gmail.com>
2020-04-04 18:45:59 +0300

    broadcast server

--- Scapy/scapyserver.py
+++ Scapy/scapyserver.py
@@ -1,5 +1,32 @@
-from scapy.all import *
+import socket
+import subprocess
+import re
+import time
+from contextlib import closing
 
-ping_packet= IP(dst="255.255.255.255")/UDP()/"ip, port"
-result = send(ping_packet, return_packets=True) # return_packets=True)
-print (result)
+def run_command(cmd):
+    return subprocess.Popen(cmd,
+        shell=True, # not recommended, but does not open a window
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE).communicate()
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+output = run_command('arp -a')[0]
+output = output.decode('utf-8')
+reg = re.compile(r'((?:\d{1,3}.){3}\d{1,3})\s+((?:[\da-f]{2}-){5}[\da-f]{2})\s+dynamic')
+ext = reg.findall(output)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = str(find_free_port())
+while True:
+    for s in ext:
+        print (s[0], str(port))
+        data = s[0]+ str(port)
+        sock.sendto((bytes(data, 'utf-8')), (s[0], 11000))
+        time.sleep(2)
